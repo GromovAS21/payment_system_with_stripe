98ba4c59e9f38d833c2bd9b20e9d2ea0863e530a
Anton Gromov <seed14112@yandex.ru>
2025-02-26 14:41:56 +0300

    [+] Добавление докера и nginx

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
