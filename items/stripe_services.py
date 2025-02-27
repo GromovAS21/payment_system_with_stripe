a342265123f1758ed942b5671b378ac8222fb7ec
Anton Gromov <seed14112@yandex.ru>
2025-02-27 16:34:24 +0300

    [+] Добавление функции скидки для заказа

--- items/stripe_services.py
+++ items/stripe_services.py
@@ -6,7 +6,7 @@ import stripe
 class StripeServices:
     """Сервис для работы с Stripe"""
 
-    def __init__(self, name: str, price: float, discount: int = None):
+    def __init__(self, name: str, price: float, discount: int | None = None):
         self.name = name
         self.price = price
         self.discount = discount
@@ -22,21 +22,21 @@ class StripeServices:
         )
         return price.get("id")
 
-    @staticmethod
-    def stripe_create_session(price_id: str) -> str:
-        """Создание Stripe сессии"""
+    def stripe_create_coupon(self) -> stripe.Coupon:
+        """Создание купона для скидки"""
+        if self.discount is not None:
+            coupon = stripe.Coupon.create(
+                duration="once",
+                percent_off=self.discount,
+            )
+            return coupon.get("id")
 
+    def stripe_create_session(self, price_id: str) -> str:
+        """Создание Stripe сессии"""
         session = stripe.checkout.Session.create(
             success_url=f"http://{os.getenv("HOST")}/success_pay/",
             line_items=[{"price": price_id, "quantity": 1}],
             mode="payment",
+            discounts=[{"coupon": self.stripe_create_coupon()}],
         )
         return session.get("id")
-
-    def stripe_create_coupon(self):
-        """Создание купона для скидки"""
-        coupon = stripe.Coupon.create(
-            duration="once",
-            percent_off=self.discount,
-        )
-        return coupon.get("id")
--- orders/models.py
+++ orders/models.py
@@ -7,7 +7,7 @@ class Discount(models.Model):
     """Модель для скидок"""
 
     name = models.CharField(max_length=100, verbose_name="Название скидки")
-    discount_percent = models.PositiveSmallIntegerField(verbose_name="Процент скидки")
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Процент скидки")
 
     def __str__(self):
         return self.name
